62df74b5b1e76da5621e93b53501bef52ae4eaeb
Eoin <e.mccoy@kainos.com>
2019-11-06 12:23:44 +0000

    Fixed routing

--- kjourney/src/app/app-routing.module.ts
+++ kjourney/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
   {
-    path: 'index', component: MainComponent
+    path: 'home', component: MainComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: '', redirectTo: 'login', pathMatch: 'full'
   }
 ];
 
