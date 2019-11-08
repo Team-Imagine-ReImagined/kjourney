4c979acdd19df85df8e9f480c150c8b724636c00
Conor McCormick <conormc@kainos.com>
2019-11-08 12:01:02 +0000

    Addressed code review changes

--- kjourney/src/app/app.module.ts
+++ kjourney/src/app/app.module.ts
@@ -9,22 +9,15 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './login/login.component';
 import { BandComponent } from './band/band.component';
-import { ViewTabsComponent } from './view-tabs/view-tabs.component';
 import { View1Component } from './view1/view1.component';
-import { View2Component } from './view2/view2.component';
-import { View3Component } from './view3/view3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    LoginComponent,
     BandComponent,
     LoginComponent,
-    ViewTabsComponent,
     View1Component,
-    View2Component,
-    View3Component
   ],
   imports: [
     NgbModule,
