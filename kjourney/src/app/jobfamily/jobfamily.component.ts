48e4760740ba31dee3b97a655b2a15f17beb782b
Maeve <m.donnelly@kainos.com>
2019-11-12 11:25:42 +0000

    Generating components

---
+++ kjourney/src/app/jobfamily/jobfamily.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jobfamily',
+  templateUrl: './jobfamily.component.html',
+  styleUrls: ['./jobfamily.component.css']
+})
+export class JobfamilyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
