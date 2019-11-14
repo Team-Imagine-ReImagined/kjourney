a9808142084a5b51b4bdb92409402ec085cdeaf5
Ryan Connolly <ryanco@Centurion.local>
2019-11-14 15:57:23 +0000

    The navbar has been hidden

--- kjourney/src/app/app.component.ts
+++ kjourney/src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from './_services/authentication.service';
 import { User } from './user';
-
+import { Capability } from 'src/models/capability';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -12,18 +13,31 @@ import { User } from './user';
 export class AppComponent {
   title = 'kjourney';
   currentUser: User;
-
+  Capabilities: Capability[];
+  private data :DataService
   constructor(
     private router: Router,
-    private authenticationService: AuthenticationService
-  ) {
+    private authenticationService: AuthenticationService,
+    public auth: AuthenticationService,
+    ) {
+      let userstat:Boolean = true;
+      const currentUser = this.auth.currentUserValue;
+      if (!currentUser) {
 
-    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
-  }
+        userstat = true;
+        //ng if -> generate top bar
+      } else{
+        userstat = false;
+        //ng if -> generate log in button at top
+      }
+    
+     
+}
 
   logout() {
     this.authenticationService.logout();
     this.router.navigate(['/login'], { queryParams: { returnUrl: this.router.url } });
-  }
+
+}
 
 }
--- kjourney/src/app/nav-bar/nav-bar.component.ts
+++ kjourney/src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ComponentFactoryResolver } from '@angular/core';
 import { DataService } from '../data.service';
 import { Capability } from '../../models/capability';
+import { AuthenticationService } from '../_services/authentication.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -11,15 +12,19 @@ export class NavbarComponent implements OnInit {
   public data: DataService;
   public capabilities: Capability[];
   public name: string[];
-  constructor(data: DataService) {
+  constructor(
+    public auth: AuthenticationService,
+    data: DataService
+    ) {
   this.data = data;
   }
-
+  
   ngOnInit(): void {
+    let Userin:Number = 0;
     this.data.getCapabilities().subscribe((n => {
       this.capabilities = n;
       }));
-
+      
   }
 
   }
